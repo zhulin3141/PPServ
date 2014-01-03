2cec76e9630ced1d4120cb245ed750bac7667a5c
zhulin <zhulin3141@gmail.com>
2014-01-03 10:52:39 +0800

    托盘菜单添加 打开当前主页

--- task_bar_icon.py
+++ task_bar_icon.py
@@ -6,6 +6,7 @@ from lang import *
 from common import *
 
 class TaskBarIcon(wx.TaskBarIcon):
+    ID_MainPage = wx.NewId()
     ID_About = wx.NewId()
     ID_Minshow = wx.NewId()
     ID_Closeshow = wx.NewId()
@@ -15,6 +16,7 @@ class TaskBarIcon(wx.TaskBarIcon):
         self.frame = frame
         self.SetIcon(wx.Icon(name='icon.ico', type=wx.BITMAP_TYPE_ICO), APPNAME)
         self.Bind(wx.EVT_TASKBAR_LEFT_DCLICK, self.OnTaskBarLeftDClick)
+        self.Bind(wx.EVT_MENU, open_main_page, id=self.ID_MainPage)
         self.Bind(wx.EVT_MENU, self.OnAbout, id=self.ID_About)
         self.Bind(wx.EVT_MENU, self.OnMinshow, id=self.ID_Minshow)
         self.Bind(wx.EVT_MENU, self.OnCloseshow, id=self.ID_Closeshow)
@@ -37,6 +39,7 @@ class TaskBarIcon(wx.TaskBarIcon):
 
     def CreatePopupMenu(self):
         menu = wx.Menu()
+        menu.Append(self.ID_MainPage, Lang().get('main_page'))
         menu.Append(self.ID_Minshow, Lang().get('hide_form'))
         menu.Append(self.ID_About, Lang().get('menu_about'))
         menu.Append(self.ID_Closeshow, Lang().get('menu_quit'))
