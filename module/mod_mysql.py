bfae9d806f2a3adcfd05b187c7fa9fb65c72cdb5
zhulin <zhulin3141@gmail.com>
2014-01-09 17:42:06 +0800

    添加mysql 的控制台

--- module/mod_mysql.py
+++ module/mod_mysql.py
@@ -3,8 +3,20 @@
 
 from common import *
 from base_module import BaseModule
+from lang import *
+import wx
 
 class Mod_Mysql(BaseModule):
     '''Mysql模块类'''
     def __init__(self, name):
         BaseModule.__init__(self, name)
+
+    def set_advt_frame(self, parent):
+        mysqlPanel = wx.Panel(parent.advtTab)
+        parent.advtTab.AddPage(mysqlPanel, self.module_name)
+
+        consoleBtn = wx.Button(mysqlPanel, -1, Lang().get('console'))
+        consoleBtn.Bind(wx.EVT_BUTTON, self.open_console)
+
+    def open_console(self, event):
+        os.system('start /D "%s" mysql -u root' % (BASE_DIR + self.path + '\\bin'))
