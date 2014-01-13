b2bb436dd44c0bcae8286fb7c19780c0e0adba16
zhulin <zhulin3141@gmail.com>
2014-01-13 10:53:49 +0800

    调整服务卸载逻辑

--- uninstall.py
+++ uninstall.py
@@ -5,5 +5,5 @@ import module
 from module.module_factory import *
 
 for mod in ModuleFactory.get_module_list():
-    if not mod.is_install():
+    if mod.is_install():
         print mod.uninstall_service()
