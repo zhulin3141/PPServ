95833d9092301c1188fb6f8dbf1c4516a34ac5bd
zhulin <zhulin3141@gmail.com>
2014-01-14 11:07:19 +0800

    分类apache配置解析

--- module/mod_apache.py
+++ module/mod_apache.py
@@ -10,26 +10,26 @@ class Mod_Apache(BaseModule):
     '''Apache模块类'''
     def __init__(self, name):
         BaseModule.__init__(self, name)
+        self.setting_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.conf_file = BASE_DIR + self.path + "\conf\httpd.conf"
+        self.parse_config_file()
+
+    def parse_config_file(self):
+        self.content = open(self.conf_file,'r').read()
+        self.listen_ports = re.findall('^Listen +([0-9]+)', self.content, re.M)
+        self.doc_root = re.findall('^DocumentRoot +(.+)', self.content, re.M)
 
     def set_advt_frame(self, parent):
         self.setting_panel = wx.Panel(parent)
+        self.setting_panel.SetSizer(self.setting_sizer)
         parent.AddPage(self.setting_panel, self.module_name)
 
-        content = open(self.conf_file,'r').read()
-        loadModuleData = re.findall('(^#*)LoadModule (.+_module)', content, re.M)
-        self.moduleList = [modName for (isLoaded, modName) in loadModuleData]
-        self.moduleLoad = [isLoaded.strip() == '' for (isLoaded, modName) in loadModuleData]
+        self.set_load_module()
 
-        self.loadList = wx.CheckListBox(self.setting_panel, -1, size=wx.DefaultSize, choices=self.moduleList)
-        self.loadList.Bind(wx.EVT_CHECKLISTBOX, self.change_module_state)
-
-        for i, isLoad in enumerate(self.moduleLoad):
-            self.loadList.Check(i, isLoad)
-
-        wx.TextCtrl(self.setting_panel, -1, self.get_default_port(), pos=(30, 20))
-        wx.TextCtrl(self.setting_panel, -1, self.get_doc_root(), pos=(50, 20))
-        sizer = wx.BoxSizer(wx.VERTICAL)
+        port = wx.TextCtrl(self.setting_panel, -1, self.get_default_port())
+        doc_root = wx.TextCtrl(self.setting_panel, -1, self.get_doc_root())
+        self.setting_sizer.Add(port)
+        self.setting_sizer.Add(doc_root)
 
     def change_module_state(self, event):
         index = event.GetInt()
@@ -40,8 +40,21 @@ class Mod_Apache(BaseModule):
         else:
             self.replace(self.conf_file, r'LoadModule ' + moduleName, '#LoadModule ' + moduleName)
 
+    def set_load_module(self):
+        loadModuleData = re.findall('(^#*)LoadModule (.+_module)', self.content, re.M)
+        self.moduleList = [modName for (isLoaded, modName) in loadModuleData]
+        self.moduleLoad = [isLoaded.strip() == '' for (isLoaded, modName) in loadModuleData]
+
+        self.loadList = wx.CheckListBox(self.setting_panel, -1, size=wx.DefaultSize, choices=self.moduleList)
+        self.loadList.Bind(wx.EVT_CHECKLISTBOX, self.change_module_state)
+
+        for i, isLoad in enumerate(self.moduleLoad):
+            self.loadList.Check(i, isLoad)
+
+        self.setting_sizer.Add(self.loadList)
+
     def get_default_port(self):
-        return "80"
+        return self.listen_ports[0]
 
     def get_doc_root(self):
-        return "d:/wamp/www/"
+        return self.doc_root[0].strip("\"' ")
