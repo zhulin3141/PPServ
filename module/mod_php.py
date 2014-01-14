457788a0850c40e65cca0cfdbc748eff57d0265c
zhulin <zhulin3141@gmail.com>
2014-01-14 17:51:12 +0800

    添加php常用项的设置

--- module/mod_php.py
+++ module/mod_php.py
@@ -4,6 +4,7 @@
 from common import *
 from base_module import BaseModule
 import wx
+import ConfigParser
 
 class Mod_Php(BaseModule):
     '''PHP模块类'''
@@ -22,6 +23,22 @@ class Mod_Php(BaseModule):
         parent.AddPage(self.setting_panel, self.module_name)
 
         self.set_load_module()
+
+        cfg = ConfigParser.SafeConfigParser()
+        cfg.read(self.conf_file)
+        all_items = [item[0] for item in cfg.items('PHP')]
+        options = ['short_open_tag', 'asp_tags', 'max_execution_time', 'memory_limit', 'error_reporting', 'display_errors']
+
+        #只获取有值的项，即没有；注释的项
+        exists_options = list(set(all_items).intersection(set(options)))
+        self.grid_sizer = wx.FlexGridSizer(rows=15, cols=2)
+        for opt in exists_options:
+            lbl = wx.StaticText(self.setting_panel, -1, opt)
+            txt = wx.TextCtrl(self.setting_panel, -1, cfg.get('PHP', opt))
+            self.grid_sizer.Add(lbl, 0, wx.ALL, 5)
+            self.grid_sizer.Add(txt, 0, wx.ALL, 3)
+
+        self.setting_sizer.Add(self.grid_sizer, 0, wx.ALL, 5)
 
     def change_module_state(self, event):
         index = event.GetInt()
