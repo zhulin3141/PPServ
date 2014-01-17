af52a9624fc99f3aa135569133b9db9f0064e4f7
zhulin <zhulin3141@gmail.com>
2014-01-17 14:33:44 +0800

    添加mysql高级设置界面

--- module/mod_mysql.py
+++ module/mod_mysql.py
@@ -13,6 +13,8 @@ class Mod_Mysql(BaseModule):
         self.conf_file = BASE_DIR + self.path + "\my.ini"
         self.parse_config_file()
 
+        self.cfg_ctr = {}
+
     def parse_config_file(self):
         self.content = open(self.conf_file,'r').read()
 
@@ -32,7 +34,17 @@ class Mod_Mysql(BaseModule):
         self.setting_panel.SetSizer(self.setting_sizer)
 
         self.opt_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.grid_sizer = wx.FlexGridSizer(rows=15, cols=2)
+        self.grid_sizer = wx.FlexGridSizer(rows=15, cols=4)
+
+        for cfg_name in self.cfg:
+            if cfg_name not in ['log_error', 'datadir']:
+                lbl = wx.StaticText(self.setting_panel, -1, cfg_name)
+                self.cfg_ctr[cfg_name] = txt = wx.TextCtrl(self.setting_panel, -1, getattr(self, cfg_name), size=(200, 20))
+                self.grid_sizer.Add(lbl, 0, wx.ALL, 5)
+                self.grid_sizer.Add(txt, 0, wx.ALL, 3)
+
+        log_btn = wx.Button(self.setting_panel, -1, Lang().get('open_log_file'))
+        log_btn.Bind(wx.EVT_BUTTON, self.open_log_file)
 
         conf_btn = wx.Button(self.setting_panel, -1, Lang().get('mysql_config_file'))
         conf_btn.Bind(wx.EVT_BUTTON, self.open_config_file)
@@ -42,6 +54,7 @@ class Mod_Mysql(BaseModule):
 
         self.handler_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.handler_sizer.AddMany([
+            (log_btn),
             (conf_btn),
             (consoleBtn)
         ])
@@ -51,6 +64,9 @@ class Mod_Mysql(BaseModule):
         self.setting_sizer.Add(self.opt_sizer, 0, wx.ALL, 5)
 
         parent.AddPage(self.setting_panel, self.module_name)
+
+    def open_log_file(self, event):
+        open_file(self.log_error)
 
     def open_config_file(self, event):
         open_file(self.conf_file)
